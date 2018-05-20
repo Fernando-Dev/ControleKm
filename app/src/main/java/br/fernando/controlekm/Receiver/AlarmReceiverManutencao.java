1a73b63bf81ec34cf5321254109c511870d0d459
Fernando-Dev <fernando_dev@outlook.com.br>
2018-05-20 18:51:30 -0300

    adicionando/alteracao de arquivos ControleKm

---
+++ app/src/main/java/br/fernando/controlekm/Receiver/AlarmReceiverManutencao.java
@@ -0,0 +1,52 @@
+package br.fernando.controlekm.Receiver;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
+
+import br.fernando.controlekm.CadastrarKm;
+import com.example.fernando.controlekm.R;
+import br.fernando.controlekm.Utilitario;
+
+public class AlarmReceiverManutencao extends BroadcastReceiver {
+    private NotificationManager notificationManager;
+    private Notification notification;
+    private PendingIntent pendingIntent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extra =intent.getExtras();
+        int alarmId = extra.getInt("ALARM_MANUTENCAO");
+        if (alarmId == 0) {
+            CadastrarKm.enableBootReceiver(context);
+            Intent notificationIntent = new Intent(context, Utilitario.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(Utilitario.class);
+            stackBuilder.addNextIntent(notificationIntent);
+            pendingIntent = stackBuilder.getPendingIntent(CadastrarKm.ALARM_MANUTENCAO,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            notification = builder.setContentTitle(context.getString(R.string.app_name))
+                    .setContentText("Você precisa fazer a manutenção da sua moto!")
+                    .setTicker(context.getString(R.string.app_name))
+                    .setSmallIcon(R.mipmap.new_ic_controlekm)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("Você precisa fazer a manutenção da sua moto!"))
+                    .setPriority(Notification.PRIORITY_MAX)
+                    .setContentIntent(pendingIntent).build();
+            notification.defaults |= notification.DEFAULT_SOUND;
+            notification.defaults |= notification.DEFAULT_VIBRATE;
+            notificationManager = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(CadastrarKm.ALARM_MANUTENCAO, notification);
+        } else {
+            notificationManager.cancel(CadastrarKm.ALARM_MANUTENCAO);
+        }
+    }
+
+}
---
+++ app/src/main/java/br/fernando/controlekm/dominio/Usuario.java
@@ -0,0 +1,75 @@
+package br.fernando.controlekm.dominio;
+
+/**
+ * Created by Flavia on 25/12/2017.
+ */
+
+public class Usuario {
+    private Integer id;
+    private String nome;
+    private String unidade;
+    private String funcao;
+    private String placa;
+    private String gerencia;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(Integer id, String nome, String unidade, String funcao, String placa, String gerencia) {
+        this.id = id;
+        this.nome = nome;
+        this.unidade = unidade;
+        this.funcao = funcao;
+        this.placa = placa;
+        this.gerencia = gerencia;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getGerencia() {
+        return gerencia;
+    }
+
+    public void setGerencia(String gerencia) {
+        this.gerencia = gerencia;
+    }
+}
